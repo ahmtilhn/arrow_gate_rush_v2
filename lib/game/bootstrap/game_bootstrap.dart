717aa838bfc840acdcd0b49c5658f0fe4b729457
ahmtilhn <119415449+ahmtilhn@users.noreply.github.com>
2026-06-24 01:40:12 +0200

    feat: support deterministic visual review states

--- lib/game/bootstrap/game_bootstrap.dart
+++ lib/game/bootstrap/game_bootstrap.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/services.dart';
 
 import '../arrow_gate_game.dart';
-import '../controllers/game_session_controller.dart';
+import '../qa/phase2_qa_mode.dart';
+import 'prototype_level.dart';
 
 class GameBootstrap {
   const GameBootstrap();
 
-  Future<ArrowGateGame> createGame({bool debugLevel = false}) async {
+  Future<ArrowGateGame> createGame({
+    bool debugLevel = false,
+    Phase2QaMode mode = Phase2QaMode.normal,
+  }) async {
     await SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    final bundle = GameSessionController(
-      debugLevel: debugLevel,
-    ).createInitialLevel();
-    return ArrowGateGame(initialBundle: bundle, debugLevel: debugLevel);
+    return ArrowGateGame(
+      initialBundle: PrototypeLevels.forQaMode(mode),
+      debugLevel: debugLevel || mode == Phase2QaMode.visualDebugLevel,
+      qaMode: mode,
+    );
   }
 }
